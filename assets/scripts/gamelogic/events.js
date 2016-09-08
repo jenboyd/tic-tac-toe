d9ba13a30c0d2e358de11b730144a87e9c23464e
jenboyd <jboyd6290@gmail.com>
2016-09-08 15:33:18 -0400

    Add Working Score Board
    Add code so score board will update when a game is won.

--- assets/scripts/gamelogic/events.js
+++ assets/scripts/gamelogic/events.js
@@ -3,13 +3,16 @@
 const api = require('./api');
 const ui = require('./ui');
 
-let gameBoard = ['','','','','','','','','',];
+let gameBoard = ['','','','','','','','',''];
 let player = 'x';
-
+let gameOver = false;
+let scoreX = 0;
+let scoreY = 0;
 
 const onNewGame = function () {
     player = 'x';
-    gameBoard = [];
+    gameBoard = ['','','','','','','','',''];
+    gameOver = false;
     $(".game-board>.row>div").removeClass("active-tile-x active-tile-y");
 };
 
@@ -21,35 +24,65 @@ const nextPlayer = function () {
     }
 };
 
-const makeMove = function () {
+const checkXWin = function () {
+  if (((gameBoard[0] === 'x') && (gameBoard[1] === 'x') && (gameBoard[2] === 'x')) ||
+      ((gameBoard[3] === 'x') && (gameBoard[4] === 'x') && (gameBoard[5] === 'x')) ||
+      ((gameBoard[6] === 'x') && (gameBoard[7] === 'x') && (gameBoard[8] === 'x')) ||
 
-  if (!($(this).hasClass('active-tile-x')) && !($(this).hasClass('active-tile-y'))){
+      ((gameBoard[0] === 'x') && (gameBoard[3] === 'x') && (gameBoard[6] === 'x')) ||
+      ((gameBoard[1] === 'x') && (gameBoard[4] === 'x') && (gameBoard[7] === 'x')) ||
+      ((gameBoard[2] === 'x') && (gameBoard[5] === 'x') && (gameBoard[8] === 'x')) ||
 
-    let tileId = this.id;
+      ((gameBoard[0] === 'x') && (gameBoard[4] === 'x') && (gameBoard[8] === 'x')) ||
+      ((gameBoard[6] === 'x') && (gameBoard[4] === 'x') && (gameBoard[2] === 'x'))){
 
-    let arrIndex = parseInt(tileId);
-
-    if (player === 'x'){
-      $(this).addClass('active-tile-x');
-      gameBoard.splice(arrIndex, 1, 'x');
-      nextPlayer ();
-    }
-    else {
-      $(this).addClass('active-tile-y');
-      gameBoard.splice(arrIndex, 1, 'y');
-      nextPlayer ();
-    }
+        return true;
   }
 };
 
-// const checkHorizontalWin = function () {
-//   if (gameBoard[0],[2],[3] === 'x') ||
-//      (gameBoard[4],[5],[6] === 'x') ||
-//      (gameBoard[7],[8],[9] === 'x') ||
-//
-//
-//   }
-// }
+const checkYWin = function () {
+  if (((gameBoard[0] === 'y') && (gameBoard[1] === 'y') && (gameBoard[2] === 'y')) ||
+      ((gameBoard[3] === 'y') && (gameBoard[4] === 'y') && (gameBoard[5] === 'y')) ||
+      ((gameBoard[6] === 'y') && (gameBoard[7] === 'y') && (gameBoard[8] === 'y')) ||
+
+      ((gameBoard[0] === 'y') && (gameBoard[3] === 'y') && (gameBoard[6] === 'y')) ||
+      ((gameBoard[1] === 'y') && (gameBoard[4] === 'y') && (gameBoard[7] === 'y')) ||
+      ((gameBoard[2] === 'y') && (gameBoard[5] === 'y') && (gameBoard[8] === 'y')) ||
+
+      ((gameBoard[0] === 'y') && (gameBoard[4] === 'y') && (gameBoard[8] === 'y')) ||
+      ((gameBoard[6] === 'y') && (gameBoard[4] === 'y') && (gameBoard[2] === 'y'))){
+
+      return true;
+  }
+};
+
+const makeMove = function () {
+  if (gameOver === false) {
+    if (!($(this).hasClass('active-tile-x')) &&
+        !($(this).hasClass('active-tile-y'))){
+          let tileId = this.id;
+          let arrIndex = parseInt(tileId);
+          if (player === 'x') {
+            $(this).addClass('active-tile-x');
+            gameBoard.splice(arrIndex, 1, 'x');
+            if (checkXWin()) {
+              gameOver = true;
+              scoreX +=1;
+              $('#score-x').html(scoreX);
+            }
+          } else {
+            $(this).addClass('active-tile-y');
+            gameBoard.splice(arrIndex, 1, 'y');
+            if (checkYWin()) {
+              gameOver = true;
+              scoreY +=1;
+              $('#score-y').html(scoreY);
+            }
+          }
+          nextPlayer ();
+          }
+    }
+};
 
 const addHandlers = () => {
   $("#newGame").on('click', onNewGame);
