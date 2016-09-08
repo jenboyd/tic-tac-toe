dde132070d50fec5e8ff9d1cd7bd1f83f2e029b5
jenboyd <jboyd6290@gmail.com>
2016-09-08 11:44:57 -0400

    Add Game Logic
    Add ability to check for active tile, change player, add 'x' or 'y' to
    gameBoard array.
    
    Change naming of active tiles.
    
    Change id of tile divs to single number.

--- assets/scripts/gamelogic/events.js
+++ assets/scripts/gamelogic/events.js
@@ -1,19 +1,59 @@
 'use strict';
 
-const app = require ('../app');
 const api = require('./api');
 const ui = require('./ui');
 
-// let gameBoard = document.getElementById('game-board');
-let data = app.host;
+let gameBoard = ['','','','','','','','','',];
+let player = 'x';
 
-const onTileSelection = function () {
-  console.log("tile clicked");
-  $(this).toggleClass("active-tile");
+
+const onNewGame = function () {
+    player = 'x';
+    gameBoard = [];
+    $(".game-board>.row>div").removeClass("active-tile-x active-tile-y");
 };
 
+const nextPlayer = function () {
+    if (player === 'x') {
+      player = 'y';
+    } else if (player === 'y') {
+      player = 'x';
+    }
+};
+
+const makeMove = function () {
+
+  if (!($(this).hasClass('active-tile-x')) && !($(this).hasClass('active-tile-y'))){
+
+    let tileId = this.id;
+
+    let arrIndex = parseInt(tileId);
+
+    if (player === 'x'){
+      $(this).addClass('active-tile-x');
+      gameBoard.splice(arrIndex, 1, 'x');
+      nextPlayer ();
+    }
+    else {
+      $(this).addClass('active-tile-y');
+      gameBoard.splice(arrIndex, 1, 'y');
+      nextPlayer ();
+    }
+  }
+};
+
+// const checkHorizontalWin = function () {
+//   if (gameBoard[0],[2],[3] === 'x') ||
+//      (gameBoard[4],[5],[6] === 'x') ||
+//      (gameBoard[7],[8],[9] === 'x') ||
+//
+//
+//   }
+// }
+
 const addHandlers = () => {
-  $(".col-xs-4").on('click', onTileSelection);
+  $("#newGame").on('click', onNewGame);
+  $(".tile").on('click', makeMove);
 };
 
 module.exports = {
